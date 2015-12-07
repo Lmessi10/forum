e898be48eb64367c1d0d638c86a89d028df7cd90
Lmessi10 <chester.abhishek@icloud.com>
2015-12-07 13:49:39 +0545

    Add comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     @post=Post.find(params[:post_id])
-    @comment=@post.comments.create(params[:comment].permit(:comment))
+    @comment=@post.comments.create(comment_params)
     @comment.user_id=current_user.id if current_user
     @comment.save
 
@@ -22,7 +22,7 @@ class CommentsController < ApplicationController
     @post=Post.find(params[:post_id])
     @comment=@post.comments.find(params[:id])
 
-    if @comment.update(params[:comment].permit(:comment))
+    if @comment.update(comment_params)
       redirect_to post_path(@post)
     else
       render 'edit'
@@ -37,4 +37,9 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
 end
